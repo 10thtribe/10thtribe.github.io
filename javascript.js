7bf6543e370fce88401fdb203d8c6df9df90894f
krysassan <u1634153@live.warwick.ac.uk>
2017-05-05 16:51:05 +0100

    AO050517_1649
    Last push before submission: added temporary styling for 'Features'
    page; tried to tidy up the cody; added buttons with glyphicons to the
    'Features' page, and undid the 'full-page button-bug' that had infested
    the Shorts.

---
+++ javascript.js
@@ -0,0 +1,33 @@
+//javascript.js
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+//$(document).ready(function(){
+    //$("ft2").click(function(){
+       // $("ft").hide();
+   // });
+   // $("#show").click(function(){
+    //   $("ft2").show();
+   // });
+ // });
+
+
+//function doIt() {
+  // $( "span,div" ).show( "slow" );
+// }
+// Can pass in function name
+// $( "button" ).click( doIt );
+ 
+//$( "form" ).submit(function( event ) {
+  //if ( $( "input" ).val() === "yes" ) {
+    //$( "p" ).show( 4000, function() {
+      //$( this ).text( "Ok, DONE! (now showing)" );
+    //});
+  //}
+  // $( "span,div" ).hide( "fast" );
